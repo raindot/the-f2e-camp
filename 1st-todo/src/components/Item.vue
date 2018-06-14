4801f9a465153ff28e5bcdf9ad163dd1ce6a9633
Raindot <sakuhime24@gmail.com>
2018-06-14 20:35:05 +0800

    刪除功能

--- 1st-todo/src/components/Item.vue
+++ 1st-todo/src/components/Item.vue
@@ -1,15 +1,20 @@
 <script>
 export default {
-  props: ["data"]
+  props: ["data"],
+  data() {
+    return {};
+  }
 };
 </script>
 
 <template>
   <li class="list-group-item d-flex align-items-center">
-    <input type="checkbox" name="" id="">
+    <input type="checkbox" name="" id="" v-model="data.done" >
     <div class="w-100 ml-2">
       <div class="d-flex w-100">
-        <div class="card-title title w-75 mr-auto">{{data}}</div>
+        <div class="card-title title w-75 mr-auto" >
+          <span :class="{'done':data.done}">{{data.message}}</span>
+        </div>
         <i class="far fa-star p-2"></i>
         <i class="fas fa-pencil-alt p-2"></i>
       </div>
@@ -25,6 +30,10 @@ export default {
 .summary {
   color: #757575;
   font-size: 10px;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
 
